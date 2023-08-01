66a0a6345035f21fc29b709630d38016ebd2b5e3
Justin Antunes-Cardoso <justin.antunes-cardoso@hotmail.com>
2023-08-01 19:15:50 -0400

    Added test for Header component.

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,3 +1,17 @@
-test('dummy test', () => {
-    expect(true).toBe(true);
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'; // Provides custom matchers like toBeInTheDocument
+import Header from './Header'; // Import your Header component
+
+// Test case
+test('renders the image in the header', () => {
+    // Arrange
+    const { getByAltText } = render(<Header />);
+
+    // Act: We don't need any action for this test case.
+
+    // Assert
+    const imageElement = getByAltText('Sudokool logo'); // Replace 'Sudokool logo' with the alt text of your image
+    expect(imageElement).toBeInTheDocument();
+
 });
